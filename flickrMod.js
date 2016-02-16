7294493a6b71861e712c6775f86e351fb372b040
MarkOtten <markottencs@gmail.com>
2016-02-16 12:35:41 -0800

    new module design

--- flickrMod.js
+++ flickrMod.js
@@ -1,27 +1,59 @@
 const https = require('https');
+const eventEmitter = require('events');
+const util = require('util');
+
+//pieces of the flickr url
+const flicUrlBegin = 'https://api.flickr.com/services/rest/?method=flickr.';
+const flicUrlEnd = '&format=json&nojsoncallback=?';
 
 
-exports.getList = function(searchTerms,numImages,apiKey,callback){
-        var data = "";
+//constructor
+function Flickr(key){
+	if(key == undefined)
+		throw new Error('please pass flickr key as constructor argument');
 
+	eventEmitter.call(this);
+	this.key = key;
+}
+util.inherits(Flickr,eventEmitter);
+module.exports = Flickr;
+
+
+
+Flickr.prototype.searchImages = function(searchTerms,numImages,callback){
+	this.getInfo(flicUrlBegin + 'photos.search&api_key=' + this.key + '&tags=' + adjustTerms(searchTerms) + '&per_page=' + numImages + flicUrlEnd,callback);
+}
+
+
+
+Flickr.prototype.getInfo = function(searchString,callback){
+	var data = "";
+        https.get(searchString, (res) => {
+                res.on('data', (d) => {
+                        console.log('should be emitting data');
+                        this.emit('data',d);
+                        data += d;
+                });
+
+                res.on('end',() => {
+                       	 this.emit('end');
+			 callback(false,data);
+                });
+
+        }).on('error', (e) => {
+                callback(e,data);
+        });
+}
+
+
+
+//if an array of arguments was passed in, return a formatted string to be used in the get request
+function adjustTerms(searchTerms){
         if( searchTerms.constructor == Array){
                 var temp = searchTerms[0];
                 for(var i = 1; i < searchTerms.length; i++)
                         temp += "&" + searchTerms[i];
                 searchTerms = temp;
         }
-
-        https.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&tags=' + searchTerms + '&per_page=' + numImages + '&format=json&nojsoncallback=?', (res) => {
-
-                res.on('data', (d) => {
-                        data += d;
-                });
-
-                res.on('end',() => {
-                        callback(false,data);
-                });
-
-        }).on('error', (e) => {
-                callback(e,data);
-        });
+	return searchTerms;
 }
